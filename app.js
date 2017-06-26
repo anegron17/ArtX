955b5cd64037d884e794d1ebd7a33c878a13f955
Giancarlo Allio <GiancarloAllio@Giancarlos-MacBook-Air.local>
2017-06-26 10:14:25 -0400

    Tried to make aboutus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var contactus = require('./routes/contactus');
+var aboutus = require('./routes/aboutus');
 var app = express();
 
 // view engine setup
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/contactus', contactus);
+app.use('/aboutus', aboutus);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
